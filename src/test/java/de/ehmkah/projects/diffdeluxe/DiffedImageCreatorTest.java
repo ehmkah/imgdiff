92dbfd228320c64514fb7edf714d48f4bd8aa68e
Michael Krausse <michael.krausse@directbox.com>
2019-07-11 14:14:51 +0200

    fix unittest - fails if expected images do not match

--- src/test/java/de/ehmkah/projects/diffdeluxe/DiffedImageCreatorTest.java
+++ src/test/java/de/ehmkah/projects/diffdeluxe/DiffedImageCreatorTest.java
@@ -3,9 +3,15 @@ package de.ehmkah.projects.diffdeluxe;
 import de.ehmkah.projects.imgdiff.DiffedImageCreator;
 import org.junit.Assert;
 import org.junit.Test;
+import sun.awt.image.ByteInterleavedRaster;
+import sun.awt.image.IntegerInterleavedRaster;
 
 import javax.imageio.ImageIO;
+import java.awt.*;
 import java.awt.image.BufferedImage;
+import java.awt.image.DataBufferByte;
+import java.awt.image.DataBufferInt;
+import java.awt.image.Raster;
 import java.io.File;
 import java.io.IOException;
 
@@ -22,14 +28,38 @@ public class DiffedImageCreatorTest {
     BufferedImage changed = readImage("/modified.png");
 
     BufferedImage actual = sut.getDifferenceImage(original, changed);
-    ImageIO.write(actual, "PNG", new File("output.png"));
     BufferedImage expected = readImage("/expected.png");
 
-    Assert.assertEquals(expected, actual);
-
+    Assert.assertTrue(compareImages(expected, actual));
   }
 
   private BufferedImage readImage(String resourcePath) throws IOException {
-    return ImageIO.read(DiffedImageCreatorTest.class.getResourceAsStream(resourcePath));
+    BufferedImage result = ImageIO.read(DiffedImageCreatorTest.class.getResourceAsStream(resourcePath));
+    return result;
+  }
+
+  /**
+   * stolen from https://stackoverflow.com/questions/11006394/is-there-a-simple-way-to-compare-bufferedimage-instances
+   */
+  private boolean compareImages(BufferedImage imgA, BufferedImage imgB) {
+    // The images must be the same size.
+    if (imgA.getWidth() != imgB.getWidth() || imgA.getHeight() != imgB.getHeight()) {
+      return false;
+    }
+
+    int width  = imgA.getWidth();
+    int height = imgA.getHeight();
+
+    // Loop over every pixel.
+    for (int y = 0; y < height; y++) {
+      for (int x = 0; x < width; x++) {
+        // Compare the pixels for equality.
+        if (imgA.getRGB(x, y) != imgB.getRGB(x, y)) {
+          return false;
+        }
+      }
+    }
+
+    return true;
   }
 }
