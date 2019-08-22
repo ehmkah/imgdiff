1fc6208ed808755e0c88baee61d63936282c7546
Michael Krauße <michael.krausse@directbox.com>
2019-08-22 21:39:45 +0200

    fixed - https://github.com/ehmkah/imgdiff/issues/15 (#18)
    * fixed - https://github.com/ehmkah/imgdiff/issues/15
    
    * updated changelog
    
    * added identical image

--- src/test/java/de/ehmkah/projects/imgdiff/DiffedImageCreatorTest.java
+++ src/test/java/de/ehmkah/projects/imgdiff/DiffedImageCreatorTest.java
@@ -8,62 +8,79 @@ import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 
+import static org.junit.Assert.assertTrue;
+
 /**
  * @author Michael Krausse (ehmkah)
  */
 public class DiffedImageCreatorTest {
 
-  private DiffedImageCreator sut = new DiffedImageCreator();
+    private DiffedImageCreator sut = new DiffedImageCreator();
 
-  @Test
-  public void testDiff() throws Exception {
-    BufferedImage original = readImage("/original.png");
-    BufferedImage changed = readImage("/modified.png");
+    @Test
+    public void testDiff() throws Exception {
+        BufferedImage original = readImage("/original.png");
+        BufferedImage changed = readImage("/modified.png");
 
-    BufferedImage actual = sut.getDifferenceImage(original, changed);
-    BufferedImage expected = readImage("/expected.png");
+        BufferedImage actual = sut.getDifferenceImage(original, changed);
+        BufferedImage expected = readImage("/expected.png");
 
-    Assert.assertTrue(compareImages(expected, actual));
-  }
-
-  @Test
-  public void testDifferentSize() throws Exception {
-    BufferedImage original = readImage("/original.png");
-    BufferedImage changed = readImage("/modifiedDifferentSize.png");
-
-    BufferedImage actual = sut.getDifferenceImage(original, changed);
-    BufferedImage expected = readImage("/expectedDifferentSize.png");
-
-    Assert.assertTrue(compareImages(expected, actual));
-  }
-
-  private BufferedImage readImage(String resourcePath) throws IOException {
-    BufferedImage result = ImageIO.read(DiffedImageCreatorTest.class.getResourceAsStream(resourcePath));
-    return result;
-  }
-
-  /**
-   * stolen from https://stackoverflow.com/questions/11006394/is-there-a-simple-way-to-compare-bufferedimage-instances
-   */
-  private boolean compareImages(BufferedImage imgA, BufferedImage imgB) {
-    // The images must be the same size.
-    if (imgA.getWidth() != imgB.getWidth() || imgA.getHeight() != imgB.getHeight()) {
-      return false;
+        assertTrue(compareImages(expected, actual));
     }
 
-    int width = imgA.getWidth();
-    int height = imgA.getHeight();
+    @Test
+    public void testDifferentSize() throws Exception {
+        BufferedImage original = readImage("/original.png");
+        BufferedImage changed = readImage("/modifiedDifferentSize.png");
 
-    // Loop over every pixel.
-    for (int y = 0; y < height; y++) {
-      for (int x = 0; x < width; x++) {
-        // Compare the pixels for equality.
-        if (imgA.getRGB(x, y) != imgB.getRGB(x, y)) {
-          return false;
+        BufferedImage actual = sut.getDifferenceImage(original, changed);
+        BufferedImage expected = readImage("/expectedDifferentSize.png");
+
+        assertTrue(compareImages(expected, actual));
+    }
+
+    @Test
+    public void testImagesHaveNoDiff() throws IOException {
+        BufferedImage original = readImage("/identical.png");
+        BufferedImage changed = readImage(("/identical.png"));
+        BufferedImage expected = readImage("/identical.png");
+
+        BufferedImage actual = sut.getDifferenceImage(original, changed);
+
+        assertTrue(compareImages(expected, actual));
+
+    }
+
+    private BufferedImage readImage(String resourcePath) throws IOException {
+        BufferedImage result = ImageIO.read(DiffedImageCreatorTest.class.getResourceAsStream(resourcePath));
+        return result;
+    }
+
+    /**
+     * stolen from https://stackoverflow.com/questions/11006394/is-there-a-simple-way-to-compare-bufferedimage-instances
+     */
+    private boolean compareImages(BufferedImage imgA, BufferedImage imgB) throws IOException {
+        // The images must be the same size.
+        if (imgA.getWidth() != imgB.getWidth() || imgA.getHeight() != imgB.getHeight()) {
+            return false;
         }
-      }
-    }
 
-    return true;
-  }
+        int width = imgA.getWidth();
+        int height = imgA.getHeight();
+
+        // Loop over every pixel.
+        for (int y = 0; y < height; y++) {
+            for (int x = 0; x < width; x++) {
+                // Compare the pixels for equality.
+                if (imgA.getRGB(x, y) != imgB.getRGB(x, y)) {
+                    ImageIO.write(imgA, "png", new File("imgA.png"));
+                    ImageIO.write(imgB, "png", new File("imgB.png"));
+
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
 }
