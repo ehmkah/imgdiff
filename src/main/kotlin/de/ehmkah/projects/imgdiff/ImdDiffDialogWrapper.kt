929b3ea498a5bb89d468ff3092727c180e9c30bb
Michael Krauße <michael.krausse@directbox.com>
2019-09-21 22:30:39 +0200

    Feature/24 (#25)
    * #5 fixed

--- src/main/kotlin/de/ehmkah/projects/imgdiff/ImdDiffDialogWrapper.kt
+++ src/main/kotlin/de/ehmkah/projects/imgdiff/ImdDiffDialogWrapper.kt
@@ -1,5 +1,6 @@
 package de.ehmkah.projects.imgdiff
 
+import com.intellij.openapi.Disposable
 import com.intellij.openapi.ui.DialogWrapper
 import org.intellij.images.editor.impl.ImageEditorManagerImpl
 import java.awt.BorderLayout
@@ -7,39 +8,33 @@ import java.awt.image.BufferedImage
 import javax.swing.JComponent
 import javax.swing.JPanel
 
+
 /**
  * Displays the diff as a dialog.
  *
  * @author Michael Krausse (ehmkah)
  */
-internal class ImdDiffDialogWrapper(private val bufferedImage: BufferedImage) : DialogWrapper(true) {
+class ImdDiffDialogWrapper(val bufferedImage: BufferedImage) : DialogWrapper(true) {
+
+    lateinit var editorForCleanup: Disposable
 
     init {
         init()
         title = "ImgDiff"
-    }// use current window as parent
+    }
 
     override fun createCenterPanel(): JComponent? {
         val result = JPanel(BorderLayout())
         val jPanel = ImageEditorManagerImpl.createImageEditorUI(bufferedImage)
+        editorForCleanup = jPanel
         result.add(jPanel, BorderLayout.CENTER)
 
         return result
     }
 
-    private fun calculateHeight(height: Int): Int {
-        return if (height > MAX_HEIGHT) MAX_HEIGHT else height + MARGIN_HEIGHT
-    }
-
-    private fun calculateWidth(width: Int): Int {
-        return if (width > MAX_WIDTH) MAX_WIDTH else width + MARGIN_WIDTH
-    }
-
-    companion object {
-
-        private val MAX_WIDTH = 1024
-        private val MAX_HEIGHT = 800
-        private val MARGIN_HEIGHT = 50
-        private val MARGIN_WIDTH = 50
+    override fun dispose() {
+        super.dispose()
+        editorForCleanup.dispose()
     }
 }
+
