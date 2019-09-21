b04f89602abb40fc474930ce3ed3de630665cdab
Michael Krausse <michael.krausse@directbox.com>
2019-09-21 16:02:03 +0200

    #5 possible solution

--- src/main/kotlin/de/ehmkah/projects/imgdiff/ImdDiffDialogWrapper.kt
+++ src/main/kotlin/de/ehmkah/projects/imgdiff/ImdDiffDialogWrapper.kt
@@ -1,45 +1,46 @@
 package de.ehmkah.projects.imgdiff
 
+import com.intellij.openapi.Disposable
 import com.intellij.openapi.ui.DialogWrapper
 import org.intellij.images.editor.impl.ImageEditorManagerImpl
 import java.awt.BorderLayout
 import java.awt.image.BufferedImage
+import java.util.*
 import javax.swing.JComponent
 import javax.swing.JPanel
+
 
 /**
  * Displays the diff as a dialog.
  *
  * @author Michael Krausse (ehmkah)
  */
-internal class ImdDiffDialogWrapper(private val bufferedImage: BufferedImage) : DialogWrapper(true) {
+class ImdDiffDialogWrapper(val bufferedImage: BufferedImage) : DialogWrapper(true) {
+
+    val result = LinkedList<JPanel>()
 
     init {
         init()
         title = "ImgDiff"
-    }// use current window as parent
+    }
 
     override fun createCenterPanel(): JComponent? {
-        val result = JPanel(BorderLayout())
+        result.add(JPanel(BorderLayout()))
         val jPanel = ImageEditorManagerImpl.createImageEditorUI(bufferedImage)
-        result.add(jPanel, BorderLayout.CENTER)
-
-        return result
+        result.get(0).add(jPanel, BorderLayout.CENTER)
+        return result.get(0)
     }
 
-    private fun calculateHeight(height: Int): Int {
-        return if (height > MAX_HEIGHT) MAX_HEIGHT else height + MARGIN_HEIGHT
-    }
-
-    private fun calculateWidth(width: Int): Int {
-        return if (width > MAX_WIDTH) MAX_WIDTH else width + MARGIN_WIDTH
-    }
-
-    companion object {
-
-        private val MAX_WIDTH = 1024
-        private val MAX_HEIGHT = 800
-        private val MARGIN_HEIGHT = 50
-        private val MARGIN_WIDTH = 50
+    // ImageEditorUI
+    override fun dispose() {
+        super.dispose()
+        val component = result.get(0).getComponent(0)
+        if (component is Disposable) {
+            component.dispose();
+        }
+        result.get(0).removeAll()
+        println("HUHU")
+        println("GÄHN")
     }
 }
+
