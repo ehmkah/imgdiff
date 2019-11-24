99acbeb3e435e3c20d81f4e4e8ce4e51f18f99c2
Michael Krauße <michael.krausse@directbox.com>
2019-11-24 21:58:20 +0100

    fixed #35 (#36)

--- src/main/kotlin/de/ehmkah/projects/imgdiff/ImgDiffBinaryDiffTool.kt
+++ src/main/kotlin/de/ehmkah/projects/imgdiff/ImgDiffBinaryDiffTool.kt
@@ -1,7 +1,6 @@
 package de.ehmkah.projects.imgdiff
 
 import com.intellij.diff.DiffContext
-import com.intellij.diff.FrameDiffTool
 import com.intellij.diff.contents.DiffContent
 import com.intellij.diff.contents.EmptyContent
 import com.intellij.diff.contents.FileContentImpl
@@ -10,7 +9,7 @@ import com.intellij.diff.requests.DiffRequest
 import com.intellij.diff.requests.SimpleDiffRequest
 import com.intellij.diff.tools.binary.BinaryDiffTool
 import com.intellij.diff.tools.binary.ThreesideBinaryDiffViewer
-import java.awt.image.BufferedImage
+import com.intellij.openapi.vfs.VirtualFile
 import java.io.ByteArrayInputStream
 import javax.imageio.ImageIO
 
@@ -22,25 +21,27 @@ class ImgDiffBinaryDiffTool : BinaryDiffTool() {
         val instance = ImgDiffBinaryDiffTool()
     }
 
-    override fun createComponent(context: DiffContext, request: DiffRequest): FrameDiffTool.DiffViewer {
+    override fun createComponent(context: DiffContext, request: DiffRequest): ThreesideBinaryDiffViewer {
         if (canShow(context, request) && (request is ContentDiffRequest)) {
             val diffContent0 = request.contents[0]
             val diffContent1 = request.contents[1]
             if (diffContent0 is FileContentImpl && diffContent1 is FileContentImpl) {
-                val bufferedImage0 = ImageIO.read(ByteArrayInputStream(diffContent0.file.contentsToByteArray()))
-                val bufferedImage1 = ImageIO.read(ByteArrayInputStream(diffContent1.file.contentsToByteArray()))
-                return createImgDiffDiffViewer(request, context, bufferedImage0, bufferedImage1, request.contentTitles[0], request.contentTitles[1])
+                return createImgDiffDiffViewer(request, context,diffContent0.file, diffContent1.file, request.contentTitles[0], request.contentTitles[1])
             }
 
         }
         throw IllegalArgumentException(request.toString())
     }
 
-    private fun createImgDiffDiffViewer(request: ContentDiffRequest, context: DiffContext, bufferedImage0: BufferedImage, bufferedImage1: BufferedImage, contentTitle0: String, contentTitle1: String): ThreesideBinaryDiffViewer {
+    private fun createImgDiffDiffViewer(request: ContentDiffRequest, context: DiffContext, filecontent0:VirtualFile, filecontent1: VirtualFile, contentTitle0: String, contentTitle1: String): ThreesideBinaryDiffViewer {
+        val bufferedImage0 = ImageIO.read(ByteArrayInputStream(filecontent0.contentsToByteArray()))
+        val bufferedImage1 = ImageIO.read(ByteArrayInputStream(filecontent1.contentsToByteArray()))
         val differenceImage = diffedImageCreator.getDifferenceImage(bufferedImage0, bufferedImage1)
-        val diffContent0 = FileContentImpl(null, ImgDiffVirtualFile(bufferedImage0))
-        val diffContentDifference = FileContentImpl(null, ImgDiffVirtualFile(differenceImage))
-        val diffContent1 = FileContentImpl(null, ImgDiffVirtualFile(bufferedImage1))
+        val diffContent0 = FileContentImpl(null, filecontent0)
+        val diffContent1 = FileContentImpl(null, filecontent1)
+
+        val diffContentDifference = FileContentImpl(null, ImgDiffVirtualFile(differenceImage, filecontent0))
+
         val myRequest = SimpleDiffRequest(request.title,
                 diffContent0, diffContentDifference, diffContent1,
                 contentTitle0, "Diff Image", contentTitle1)
