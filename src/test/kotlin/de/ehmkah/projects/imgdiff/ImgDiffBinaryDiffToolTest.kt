b5206026c11db2ebb9accba5c3ddb9067f944996
Michael Krausse <michael.krausse@directbox.com>
2020-04-12 22:02:02 +0200

    fixed unit-test

--- src/test/kotlin/de/ehmkah/projects/imgdiff/ImgDiffBinaryDiffToolTest.kt
+++ src/test/kotlin/de/ehmkah/projects/imgdiff/ImgDiffBinaryDiffToolTest.kt
@@ -1,9 +1,9 @@
 package de.ehmkah.projects.imgdiff
 
 import com.intellij.diff.contents.DiffContent
+import com.intellij.openapi.fileTypes.FileType
 import com.nhaarman.mockitokotlin2.doReturn
 import com.nhaarman.mockitokotlin2.mock
-import org.intellij.images.fileTypes.impl.ImageFileType
 import org.junit.Test
 import kotlin.test.assertTrue
 
@@ -13,8 +13,12 @@ internal class ImgDiffBinaryDiffToolTest {
 
     @Test
     fun testIsImage() {
+        val contentTypeMock = mock<FileType> {
+            on { name } doReturn "Image"
+        }
+
         val request = mock<DiffContent> {
-            on { contentType } doReturn ImageFileType.INSTANCE
+            on { contentType } doReturn contentTypeMock
         }
         assertTrue(sut.isValidImage(request))
     }
